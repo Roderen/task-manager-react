1a6a58a67a9e1576a171a694ecde89c8bf8527e8
Roderen <56632427+Roderen@users.noreply.github.com>
2026-05-21 17:34:35 +0200

    Update useSocket.ts

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,8 +1,8 @@
-import { io } from 'socket.io-client';
+import { io } from 'socket.io-client'
 
-const URL = import.meta.env.VITE_API_URL
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL
 
-export const socket = io(URL, {
+export const socket = io(SOCKET_URL, {
     withCredentials: true,
-    autoConnect: false
+    autoConnect: false,
 })
